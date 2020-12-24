9c4fb26ff4e19894d8403d954df5c1421dcef63c
Julian Nino <ingsistemasjulian@gmail.com>
2020-12-24 08:18:40 -0500

    Getters y setters al reproductor de video

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -6,8 +6,29 @@ function MediaPlayer(config) {
 }
 
 MediaPlayer.prototype._initPlugins = function() {
+    const player = {
+        // this hace referencia a player que es el
+        // objeto que ejecuta esta parte del codigo
+        play: () => this.play(),
+        pause: () => this.pause(),
+        media: this.media, // debe ser definido media, porque se pierde su referencia
+
+        // get permite acceder a la propiedad muted desde fuera
+        // en este caso "if (!player.muted)"" accede desde Autoplay.js
+        get muted() {
+            return this.media.muted;
+        },
+
+        // set permite modificar una propiedad desde fuera
+        // en este caso  "player.muted = true" desde Autoplay.js
+        // donde true es el valor que se le va dar a la propiedad
+        set muted(value) {
+            this.media.muted = value;
+        },
+    };
+
     this.plugins.forEach(plugin => {
-        plugin.run(this);
+        plugin.run(player);
     });
 };
 
